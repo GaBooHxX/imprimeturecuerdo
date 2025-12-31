cb89ef213346c0fa46d839dbe16203671e109929
GaBooHxX <gabriel.amadorf@gmail.com>
2025-12-31 17:53:17 -0300

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+// assets/js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYCf3adJckWwR0IQlQsU-rzBya2rta89Q",
+  authDomain: "imprimeturecuerdo.firebaseapp.com",
+  projectId: "imprimeturecuerdo",
+  storageBucket: "imprimeturecuerdo.firebasestorage.app",
+  messagingSenderId: "960020060014",
+  appId: "1:960020060014:web:41385e1a91a62cb921137f"
+};
